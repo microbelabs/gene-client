dcb71d67d057622ff467719c8fdb9c30ab588d1c
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2017-12-10 18:06:31 +0700

    Add: more routes
    - games
    - marketplace
    - community
    
    Update Wallet routes to exact path

--- app/routes.tsx
+++ app/routes.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import Wallet from './containers/Wallets';
+import Community from './containers/Community';
+import Games from './containers/Games';
+import Marketplace from './containers/Marketplace';
 
 export default () => (
   <App>
     <Switch>
-      <Route path="/" component={Wallet} />
+      <Route exact path="/" component={Wallet} />
+      <Route path="/games" component={Games} />
+      <Route path="/marketplace" component={Marketplace} />
+      <Route path="/community" component={Community} />
     </Switch>
   </App>
 );
