a1769e7b217d40a08edfcaa45136eb59cb766972
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2017-12-10 18:09:14 +0700

    Fix: Initial Containers
    Add dummy data to initial containers.
    - Games
    - Marketplace
    - Community

---
+++ app/containers/Community/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Fragment = styled.div``;
+
+export default class Community extends React.Component {
+  render() {
+    return (
+      <Fragment>Community</Fragment>
+    );
+  }
+}
---
+++ app/containers/Marketplace/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Fragment = styled.div ``;
+
+export default class Marketplace extends React.Component {
+  render() {
+    return (
+      <Fragment>Marketplace</Fragment>
+    );
+  }
+}
