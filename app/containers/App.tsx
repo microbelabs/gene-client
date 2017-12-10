ffc48c73d719ba83946aeb989d217ca9300f8611
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-12-10 12:07:30 +0700

    fix: app container

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Navbar from '../components/Navbar';
+
+// Temporary
+const Fragment = styled.div``
+
+const Container = styled.div`
+  padding: 15px 30px;
+`;
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <Navbar />
-        {this.props.children}
-      </div>
+        <Container>{this.props.children}</Container>
+      </Fragment>
     );
   }
 }
