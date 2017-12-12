ceac7c79e5f680d8d04ab4bd8be6192f05fbf213
Ribhararnus Pracutian <oknoorap@gmail.com>
2017-12-13 06:27:27 +0700

    add: communities related icon resource and components

---
+++ app/components/SVGIcon/Facebook.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+const Facebook = (props: any) => (
+  <svg
+    width={33}
+    height={33}
+    viewBox="0 0 33 33"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <title>Facebook</title>
+    <use xlinkHref="#facebook0_fill" fill="#333333" />
+    <defs>
+      <path
+        id="facebook0_fill"
+        d="M30.25 0H2.75C1.21 0 0 1.21 0 2.75v27.5C0 31.79 1.21 33 2.75 33h27.5c1.54 0 2.75-1.21 2.75-2.75V2.75C33 1.21 31.79 0 30.25 0zM16.5 31.9v-11h-4.4v-4.4h4.4v-3.85c0-3.135 2.42-5.775 5.5-6.05h4.95V11h-4.4a1.62 1.62 0 0 0-1.65 1.65v3.85h5.5v4.4h-5.5v11h-4.4zm15.4-1.65a1.62 1.62 0 0 1-1.65 1.65H22V22h5.5v-6.6H22v-2.75c0-.33.22-.55.55-.55h5.5V5.5h-6.105c-3.685.33-6.545 3.465-6.545 7.15v2.75H11V22h4.4v9.9H2.75a1.62 1.62 0 0 1-1.65-1.65V2.75c0-.935.715-1.65 1.65-1.65h27.5c.935 0 1.65.715 1.65 1.65v27.5z"
+      />
+    </defs>
+  </svg>
+);
+
+export default Facebook;
+
---
+++ app/components/SVGIcon/Slack.tsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+
+const Slack = (props: any) => (
+  <svg
+    width={33}
+    height={33}
+    viewBox="0 0 33 33"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <title>Slack</title>
+    <use xlinkHref="#slack0_fill" transform="translate(15.665)" fill="#333" />
+    <use
+      xlinkHref="#slack1_fill"
+      transform="translate(3.886 3.827)"
+      fill="#333"
+    />
+    <use
+      xlinkHref="#slack2_fill"
+      transform="translate(3.827 15.665)"
+      fill="#333"
+    />
+    <use
+      xlinkHref="#slack3_fill"
+      transform="translate(8.822 20.6)"
+      fill="#333"
+    />
+    <use
+      xlinkHref="#slack4_fill"
+      transform="translate(20.6 16.772)"
+      fill="#333"
+    />
+    <use xlinkHref="#slack5_fill" transform="translate(0 3.887)" fill="#333" />
+    <use
+      xlinkHref="#slack6_fill"
+      transform="translate(4.994 8.822)"
+      fill="#333"
+    />
+    <use
+      xlinkHref="#slack7_fill"
+      transform="translate(16.772 4.994)"
+      fill="#333"
+    />
+    <defs>
+      <path
+        id="slack0_fill"
+        d="M5.736 2.032A2.94 2.94 0 1 0 .145 3.849l7.6 23.386a2.94 2.94 0 0 0 3.577 1.823c1.56-.45 2.511-2.106 2.013-3.639L5.736 2.032z"
+      />
+      <path
+        id="slack1_fill"
+        d="M5.736 2.032A2.94 2.94 0 0 0 .145 3.849l7.601 23.386a2.94 2.94 0 0 0 3.576 1.823c1.56-.45 2.511-2.106 2.013-3.639L5.736 2.032z"
+      />
+      <path
+        id="slack2_fill"
+        d="M27.141 5.736A2.94 2.94 0 1 0 25.324.145L1.938 7.746a2.94 2.94 0 0 0-1.823 3.576c.45 1.56 2.107 2.51 3.639 2.013L27.14 5.736"
+      />
+      <path
+        id="slack3_fill"
+        d="M1.817 7.406l5.59-1.816L5.59 0 0 1.817l1.817 5.59"
+      />
+      <path
+        id="slack4_fill"
+        d="M1.817 7.407l5.59-1.816L5.59 0 0 1.817l1.817 5.59z"
+      />
+      <path
+        id="slack5_fill"
+        d="M27.141 5.736A2.94 2.94 0 1 0 25.324.145L1.938 7.746a2.939 2.939 0 0 0-1.822 3.576c.448 1.56 2.105 2.51 3.638 2.013l23.387-7.599"
+      />
+      <path
+        id="slack6_fill"
+        d="M1.817 7.406l5.59-1.816L5.591 0 0 1.817l1.817 5.59"
+      />
+      <path
+        id="slack7_fill"
+        d="M1.817 7.408l5.59-1.817A67506.4 67506.4 0 0 0 5.591 0L0 1.817l1.817 5.59z"
+      />
+    </defs>
+  </svg>
+);
+
+export default Slack;
+
